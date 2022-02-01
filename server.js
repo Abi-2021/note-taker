93442d23271222ec6a1b8eaee282712fcb887377
Abirami Theepan <abira0607@gmail.com>
2022-02-01 15:06:04 -0500

    implement-api

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
+const fs = require('fs');
 const express = require('express');
-const PORT = 5001;
+
+const uuid = require('uuid');
+
+
+const PORT = 5000;
 
 const app = express();
+app.use(express.json())
+app.use(express.static("public"));
 
+const readFile = function () {
+    return JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+}
 
-app.listen(PORT,function () {
-    console.log(`Server is running on PORT ${PORT}`)
+const writeFile = function (data, res) {
+    fs.writeFileSync('./db/db.json', JSON.stringify(data), err => {
+        if (err) return res.status(500).json({success: false});
+    })
+}
+
+// Api routes
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.status(200).json(notes)
+})
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+    if (!note)
+        return res.status(400).json({success: false})
+    const notes = readFile();
+    const previousNotes = [...notes]
+    const id = uuid.v4();
+    previousNotes.push({...note, id})
+    writeFile(previousNotes, res);
+    res.status(201).json(note);
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile('./public/notes.html', {root: __dirname})
+})
+
+app.get('*', (req, res) => {
+    res.sendFile('./public/index.html', {root: __dirname})
+})
+
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`)
 });
